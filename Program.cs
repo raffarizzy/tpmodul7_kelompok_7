c8ba5038407335494cce55ddeeb2c17a1eee59eb
naufa <naufal.mdzulfik@gmail.com>
2025-04-07 22:11:57 +0700

    menambahkan class KuliahMahasiswa1030223300021

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace tpmodul7_kelompok_7
         static void Main(string[] args)
         {
             DataMahasiswa1030223300021.ReadJSON();
+            KuliahMahasiswa1030223300021.ReadJSON();
         }
     }
 }
